8f31ca3f1e4178dd162856b4e42e2ce1daf2ac9c
MustyX23 <mustyx900@gmail.com>
2023-08-17 19:09:20 +0300

    Create RoundButton.cs

---
+++ CafeManagementSystem/RoundButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CafeManagementSystem
+{
+    public class RoundButton : Button
+    {
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(grPath);
+            base.OnPaint(e);
+        }
+    }
+}
