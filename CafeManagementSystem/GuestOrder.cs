f4db8e3a093a04364c26fb5715bba2434e7d0cf4
MustyX23 <mustyx900@gmail.com>
2023-08-18 16:00:01 +0300

    Create GuestOrder.cs

---
+++ CafeManagementSystem/GuestOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CafeManagementSystem
+{
+    public partial class GuestOrder : Form
+    {
+        public GuestOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void ItemForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
