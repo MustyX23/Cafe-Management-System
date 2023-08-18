e62b1d5144f0f8aee1bd122f1e4f9b1480123c33
MustyX23 <mustyx900@gmail.com>
2023-08-18 17:29:52 +0300

    Create ItemsForm.cs

---
+++ CafeManagementSystem/ItemsForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CafeManagementSystem
+{
+    public partial class ItemsForm : Form
+    {
+        public ItemsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
