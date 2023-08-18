116e31097032c2d73d8ec42c61ded7ff579a0f2e
MustyX23 <mustyx900@gmail.com>
2023-08-18 17:29:57 +0300

    Create UserOrder.cs

---
+++ CafeManagementSystem/UserOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CafeManagementSystem
+{
+    public partial class UserOrder : Form
+    {
+        public UserOrder()
+        {
+            InitializeComponent();
+        }
+    }
+}
