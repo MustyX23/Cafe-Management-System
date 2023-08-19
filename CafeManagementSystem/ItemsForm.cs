3bb2c7ee27795d575a09e34a7d7f366d87849078
MustyX23 <mustyx900@gmail.com>
2023-08-19 12:26:53 +0300

    Update ItemsForm.cs

--- CafeManagementSystem/ItemsForm.cs
+++ CafeManagementSystem/ItemsForm.cs
@@ -29,5 +29,26 @@ namespace CafeManagementSystem
         {
             Application.Exit();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            UserOrder order = new UserOrder();
+            order.Show();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form1 login = new Form1();
+            login.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            UsersForm users = new UsersForm();
+            users.Show();
+        }
     }
 }
